f5dbbf4f81adf4ac9910d89a9db71b92323434c1
brandent25 <brandent35@tamu.edu>
2025-12-05 02:08:53 -0600

    sync

--- bencode_parser/bencodeParser.cpp
+++ bencode_parser/bencodeParser.cpp
@@ -2,15 +2,43 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <cctype>
 #include <fstream>
+#include <map>
 using namespace std;
+
+class ReadFile{
+  int index;
+  ifstring file;
+  public:
+    ReadFile(const string fileName&, index(0)){
+      file.open(fileName, ios::in){
+        if(!file.is_open){
+          throw runtime_error("Failed to open File");
+        }
+        file.close();
+      }
+    }
+  char getNextChar(){
+    file.seekg(index, ios::beg);
+    
+    char c;
+    file.get(c);
+
+    if
+  }
+
+
+
+}
+
 
 enum Types{
   Integer,
   String,
   List,
   Dictionary
-}
+};
 
 
 struct bencodeNode{
@@ -23,13 +51,38 @@ struct bencodeNode{
   union{
     int remainingLength;
     bool isReadingKey;
-
   }typeData;
+};
+
+
+void checkType(char ch){
+  if(isdigit(ch)){
+    cout << "number";
+  }
+  else{
+    switch(ch){
+      case 'i':
+        cout << "integer";
+        break;
+      case 'd':
+        break;
+      case 'l':
+        break;
+    }
+  }
 }
 
-void parseBencodeInteger(){
-  
+void parseBencodeInteger(char ch){
+  bencodeNode node;
+  while(ch != 'e'):
+    bencodeNode.list.push_back(ch);
+    
+
 }
+
+string getChar();
+
+
 
 void parseBencode(string torrentFile){
   ifstream file(torrentFile);
@@ -38,7 +91,7 @@ void parseBencode(string torrentFile){
   }
   char ch; 
   while(file.get(ch)){
-    cout << ch;
+    checkType(ch);
   }
   file.close();
   
