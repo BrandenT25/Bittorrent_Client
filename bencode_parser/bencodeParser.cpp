1e4bec059be75f90163b01f4c5dd507985480ecd
Branden <brandent35@tamu.edu>
2026-02-13 21:20:48 -0600

    restarted project

--- bencode_parser/bencodeParser.cpp
+++ bencode_parser/bencodeParser.cpp
@@ -5,100 +5,62 @@
 #include <cctype>
 #include <fstream>
 #include <map>
+
 using namespace std;
+namespace Bencode {
+    const char INT_BEGIN = 'i';
+    const char INT_END = 'e';
+    const char LIST_BEGIN = 'l';
+    const char LIST_END = 'e';
+    const char DICT_BEGIN = 'd';
+    const char DICT_END = 'e';
+    const char STRING_DELIN = 'e';
+}
 
-class ReadFile{
-  int index;
-  ifstream file;
-  public:
-    ReadFile(const string &fileName){
-      index = 0;
-      file.open(fileName, ios::in);
-        if(!file.is_open()){
-          throw runtime_error("Failed to open File");
-        }
+struct bencodeValue;
+
+
+struct bencodeList{
+    vector<bencodeValue> listItems;
+};
+
+
+struct{
+   map<string, bencodeValue> dictItems;
+}bencodeDict;
+
+struct bencodeValue{
+    std::variant<long long, string, bencodeList, bencodeDict>;
+}
+
+string parseInt(const string bencodeInt){
+    string finalBencodeInt;
+    for(const char& c: bencodeInt){
+        if(c != 'i' && c != 'e'){
+            bencodeString += c;
+        }else if(c == 'e'){
+            return finalBencodeInt;
+        } 
     }
-  char getNextChar(){
-      file.seekg(index, ios::beg);
-      char c;
-      file.get(c);
-      if(!file){
-        cout << "error reading file";
-      }
-      index += 1;
-      return c;
-  }
-};
+}
 
-
-enum Types{
-  Integer,
-  String,
-  List,
-  Dictionary
-};
-
-struct bencodeNode{
-  Types type;
-  string value;
-
-  vector<bencodeNode> list;
-  map<string, bencodeNode> dictItems;
-
-  union{
-    int remainingLength;
-    bool isReadingKey;
-  }typeData;
-};
-
-
-void checkType(char ch){
-  if(isdigit(ch)){
-    cout << "number";
-  }
-  else{
-    switch(ch){
-      case 'i':
-        cout << "integer";
-        break;
-      case 'd':
-        break;
-      case 'l':
-        break;
+string parseString(const string bencodeString, const int size){
+    string finalBencodeString;
+    size_t index = ;
+    while(index < 3+size){
+        finalBencodeString += bencodeString[index];
+        index++;
     }
-  }
+    return finalBencodeString;
 }
 
-void parseBencodeInteger(char ch){
-  bencodeNode node;
-  while(ch != 'e'){
-    break;
-  }
 
-}
-
-string getChar();
-
-
-
-void parseBencode(ReadFile& torrentFile){
-  char ch = torrentFile.getNextChar();
-  checkType(ch);
-}
-  
-
-
-string inputHandler(){
-  string file;
-  cout << "What file will you like to parse:  ";
-  cin >> file;
-  return file;
-}
 
 
 int main(){
-  string file = inputHandler();
-  ReadFile torrentFile(file);
-  parseBencode(torrentFile);
-}
 
+
+
+
+    return 0;
+}
